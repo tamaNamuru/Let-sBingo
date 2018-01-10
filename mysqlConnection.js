7d44b5e83059c3633f4c9cb0d66d0fe07bd18414
tamaNamuru <arata1907@gmail.com>
2018-01-10 15:12:55 +0900

    add new files

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,7 +1,7 @@
 ﻿var Connection = require('tedious').Connection;
 
 var dbConfig = {
-	user: 'master',
+	userName: 'master',
 	password: 'ikkKtmksrsr7',
 	server: 'keserasera.database.windows.net',
 	
@@ -10,13 +10,35 @@ var dbConfig = {
 
 var connection = new Connection(dbConfig);
 
-module.exports = connection;
+//module.exports = connection;
 
 connection.on('connect', function(err) {
-	if(err) {
-		console.log(err);
-	}else {
-		console.log("suce");
-		connection.close();
-	}
+	console.log("Connected");
+	executeStatement();
 });
+var Request = require('tedious').Request;  
+var TYPES = require('tedious').TYPES;
+
+function executeStatement() {
+	request = new Request("SELECT * FROM room;",function(err) {
+		if(err) {
+			console.log(err);
+		}
+	});
+	var result = "";
+	request.on('row', function(columns) {
+		columns.forEach(function(column) {
+			if (column.value === null) {
+				console.log('NULL');
+			} else {
+				result+= column.value + " ";
+			}
+		});
+		console.log(result);
+		result = "";
+	});
+	request.on('done', function(rowCount, more) {  
+        	console.log(rowCount + ' rows returned');  
+        });  
+        connection.execSql(request);  
+}
