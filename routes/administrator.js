afb4fabdbe9c45d72c11ce1fb57b2cd9708ace27
tamaNamuru <arata1907@gmail.com>
2018-01-11 14:17:33 +0900

    add new files

--- routes/administrator.js
+++ routes/administrator.js
@@ -53,7 +53,6 @@ router.post('/signup', function(req, res, next) {
                     console.log(column);
                     result+= column.value;
                 });
-                console.log(column);
                 console.log("result" + result);
                 if(columns.value != null) {
                     res.render('新規作成画面', { error: "すでに同じ名前の部屋があります。"});
