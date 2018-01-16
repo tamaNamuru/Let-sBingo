a834e40303e3c4681f2a03536240b7c75b744b19
tamaNamuru <arata1907@gmail.com>
2018-01-16 10:17:01 +0900

    add new files

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,13 +1,15 @@
-﻿var mysql = require('pg');
+﻿/*var mysql = require('pg');
 
 var dbConfig = {
 	host: 'ec2-54-235-244-185.compute-1.amazonaws.com',
 	user: 'tozwceffemspzk',
 	password: '5da6c1cc968a378a0d50dcb18ed006418f7d1da596405fd30cefa7610e999115',
 	database: 'd59bruvr6nrgsa'
-};
+};*/
+var pg = require('pg');
 
-const client = new mysql.Client(dbConfig);
+var conString = "postgres://tozwceffemspzk:5da6c1cc968a378a0d50dcb18ed006418f7d1da596405fd30cefa7610e999115@ec2-54-235-244-185.compute-1.amazonaws.com:5432/d59bruvr6nrgsa";
+var client = new pg.Client(conString);
 //var connection = mysql.createConnection(dbConfig);
 
 module.exports = client;
