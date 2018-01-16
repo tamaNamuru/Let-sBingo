f5ba8fbf99472b1510ff44ecac576b65fc204016
tamaNamuru <arata1907@gmail.com>
2018-01-16 10:32:26 +0900

    add new files

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -6,10 +6,15 @@ var dbConfig = {
 	password: '5da6c1cc968a378a0d50dcb18ed006418f7d1da596405fd30cefa7610e999115',
 	database: 'd59bruvr6nrgsa'
 };*/
-var pg = require('pg');
+const pg = require('pg');
 
-var conString = "postgres://tozwceffemspzk:5da6c1cc968a378a0d50dcb18ed006418f7d1da596405fd30cefa7610e999115@ec2-54-235-244-185.compute-1.amazonaws.com:5432/d59bruvr6nrgsa";
-var client = new pg.Client(conString);
+const conString = "postgres://tozwceffemspzk:5da6c1cc968a378a0d50dcb18ed006418f7d1da596405fd30cefa7610e999115@ec2-54-235-244-185.compute-1.amazonaws.com:5432/d59bruvr6nrgsa";
+const client = new pg.Client(conString);
 //var connection = mysql.createConnection(dbConfig);
-
+client.connect(err => {
+	if(err) throw err;
+	else {
+		console.log("success");
+	}
+});
 module.exports = client;
