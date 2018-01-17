3caf21a9ae583950857ec0b8ae88718aae0c5a7a
tamaNamuru <arata1907@gmail.com>
2018-01-17 16:39:02 +0900

    add new files

--- routes/prize.js
+++ routes/prize.js
@@ -33,7 +33,8 @@ router.get('/show', function(req, res, next) {
         });
     
     request.on('row', (columns) => {
-        res.render('reading', { prizes: columns.value });
+        console.log(columns);
+        res.render('reading', { prizes: columns });
     });
     request.addParameter('ID', TYPES.NChar, req.session.user.id);
     connection.execSql(request);
