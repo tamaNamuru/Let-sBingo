80653566d50576c6aedf477e6df13cabbc4b6a0e
tamaNamuru <arata1907@gmail.com>
2018-01-17 13:52:58 +0900

    add new files

--- routes/administrator.js
+++ routes/administrator.js
@@ -6,8 +6,9 @@ var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 var async = require('async');
 
-var select = 'SELECT name FROM room WHERE room_id = ? AND password = ?';
+var select = 'SELECT name FROM room WHERE room_id = @ID AND password = @Password;';
 
+var nameselect = 'SELECT name FROM room WHERE name = @Name;';
 var idselect = 'SELECT room_id FROM room;';
 var insert = 'INSERT INTO room(room_id, password, name) VALUES(@ID, @Password, @Name);';
 var insert_card = "INSERT INTO card(room_id) VALUES(@ID);";
@@ -30,88 +31,84 @@ router.post('/signup', function(req, res, next) {
         res.redirect('signup');
         return;
     }
-    //connection.on('connect', function(error) {
-        let id = ('000' + Math.floor(Math.random() * (10000))).slice(-4);
-        async.waterfall([
-            (next) => {
-                //データベースに登録
-                console.log("1");
-                let request = new Request(
-                'SELECT name FROM room WHERE name = @Name;',
-                (err, rowCount, rows) => {
-                    console.log("2");
-                    if(err || rowCount > 0) {
-                        res.render('新規作成画面', { error: "すでに同じ名前の部屋があります。"});
-                        return;
-                    } else {
-                        next(null);
-                    }
-                });
-                request.addParameter('Name', TYPES.NVarChar, req.body.name);
-                connection.execSql(request);
-            },
-            (next) => {
-                //room_idを取得
-                let request = new Request(idselect,
-                (err, rowCount, rows) => {
-                    if(err) {
-                        next(err);
-                    } else {
-                        let idSet = new Set();
-                        for(let re in rows) {
-                            idSet.add(re.room_id);
-                        }
-                        //idが重複していれば新しく作る
-                        while(idSet.has(id)) id = ('000' + Math.floor(Math.random() * (10000))).slice(-4);
-                        next(null);
-                    }
-                });
-                connection.execSql(request);
-            },
-            (next) => {
-                //追加
-                 //room_idを取得
-                let request = new Request(insert,
-                (err, rowCount, rows) => {
-                    if(err) {
-                        next(err);
-                    } else {
-                        next(null);
-                    }
-                });
-                request.addParameter('ID', TYPES.NChar, id);
-                request.addParameter('Password', TYPES.NVarChar, req.body.password);
-                request.addParameter('Name', TYPES.NVarChar, req.body.name);
-                connection.execSql(request);
-            },
-            (next) => {
-                //写真用のフォルダ作成
-                fs.mkdir('../public/projects/' + id, function (err) {
-                    if(err) console.log(id + "folder error");console.log(err);
-                });
-                //使用するビンゴカードの登録(ここではstandard.cssが設定される)
-                let request = new Request(insert_card,
-                (err, rowCount, rows) => {
-                    if(err) {
-                        console.log(id + "insert table card error");
-                        next(err);
-                    } else {
-                        next(null);
-                    }
-                });
-                request.addParameter('ID', TYPES.NChar, id);
-                connection.execSql(request);                    
-            }],
-            (err) => {
-                if(err){
-                    res.redirect('signup');
-                }else{
-                    req.session.user = {id: id, name: req.body.name, administrator: true};
-                    res.redirect('../');
+    let id = ('000' + Math.floor(Math.random() * (10000))).slice(-4);
+    async.waterfall([
+        (next) => {
+            //データベースに登録
+            let request = new Request(
+            nameselect,
+            (err, rowCount, rows) => {
+                if(err || rowCount > 0) {
+                    res.render('新規作成画面', { error: "すでに同じ名前の部屋があります。"});
+                    return;
+                } else {
+                    next(null);
                 }
+            });
+            request.addParameter('Name', TYPES.NVarChar, req.body.name);
+            connection.execSql(request);
+        },
+        (next) => {
+            //room_idを取得
+            let request = new Request(idselect,
+            (err, rowCount, rows) => {
+                if(err) {
+                    next(err);
+                } else {
+                    let idSet = new Set();
+                    for(let re in rows) {
+                        idSet.add(re.room_id);
+                    }
+                    //idが重複していれば新しく作る
+                    while(idSet.has(id)) id = ('000' + Math.floor(Math.random() * (10000))).slice(-4);
+                    next(null);
+                }
+            });
+            connection.execSql(request);
+        },
+        (next) => {
+            //追加
+             //room_idを取得
+            let request = new Request(insert,
+            (err, rowCount, rows) => {
+                if(err) {
+                    next(err);
+                } else {
+                    next(null);
+                }
+            });
+            request.addParameter('ID', TYPES.NChar, id);
+            request.addParameter('Password', TYPES.NVarChar, req.body.password);
+            request.addParameter('Name', TYPES.NVarChar, req.body.name);
+            connection.execSql(request);
+        },
+        (next) => {
+            //写真用のフォルダ作成
+            fs.mkdir('../public/projects/' + id, function (err) {
+                if(err) console.log(id + "folder error");
+            });
+            //使用するビンゴカードの登録(ここではstandard.cssが設定される)
+            let request = new Request(insert_card,
+            (err, rowCount, rows) => {
+                if(err) {
+                    console.log(id + "insert table card error");
+                    next(err);
+                } else {
+                    next(null);
+                }
+            });
+            request.addParameter('ID', TYPES.NChar, id);
+            connection.execSql(request);                    
+        }],
+        (err) => {
+            if(err){
+                res.redirect('signup');
+            }else{
+                req.session.user = {id: id, name: req.body.name, administrator: true};
+                res.redirect('../');
             }
-        );
-    //});
+        }
+    );
 });
 
 //管理者のログイン
@@ -140,14 +137,19 @@ router.post('/login', function(req, res, next) {
 		return;
 	}
 	
-	connection.query(select, [id, pass], function(error, results, fields) {
-		if(error || results.length != 1) {
-			res.render('adminlogin', {error: '入力が正しくありません。'});
-		} else {
-			req.session.user = {id: id, name: results[0].name, administrator: true};
-			res.redirect('../');
-		}
-	});
+    let request = new Request(
+        select,
+        (err, rowCount, rows) => {
+            if(error || rowCount != 1) {
+                res.render('adminlogin', {error: '入力が正しくありません。'});
+            } else {
+                req.session.user = {id: id, name: rows[0].name, administrator: true};
+                res.redirect('../');
+            }
+        });
+    request.addParameter('ID', TYPES.NChar, id);
+    request.addParameter('Password', TYPES.NVarChar, pass);
+    connection.execSql(request);
 });
 
 module.exports = router;
