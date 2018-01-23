1e3a1ac28183bc1744c8d2d77a0d6e553072684d
tamaNamuru <arata1907@gmail.com>
2018-01-23 14:06:57 +0900

    add new files

--- public/javascripts/attack25guest.js
+++ public/javascripts/attack25guest.js
@@ -73,7 +73,7 @@ $(".tiketd").hover(function(){
 		$(this).css({"background-image":'url("/images/tike.png")'});
 	}
 });
-        $('#setumeiok').text("Your Turn!");
+        $('#setumeiok').val("Your Turn!");
         $('#setumeiok').css({"color":'tomato'});
         $('#setumeiok').prop("disabled", false);
     });
